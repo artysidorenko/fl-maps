b5502a8e83a1f68d06798731dbd908931e304ddf
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-05-14 07:24:07 +0100

    Repaired existing meteor method tests

--- tests/unit-tests/meteorMocks/mdg:validated-method.js
+++ tests/unit-tests/meteorMocks/mdg:validated-method.js
@@ -1,9 +1,18 @@
 /**
  * @author: Arty S
  * 
- * Attempting to create a mock of the meteor package mdg:validatedmethod.
+ * A basic mock of the meteor package mdg:validatedmethod.
+ * Purpose is to test the class is being used correctly
+ * Hence we don't need to include any real functionality
+ * Only to assign to 'this' the class properties passed into the constructor
+ * 
  */
 
-const mock = {}
-
-export default mock;
+export class ValidatedMethod {
+  constructor(options) {
+    this.name = options.name
+    this.mixins = options.mixins
+    this.validate = options.validate
+    this.run = options.run
+  }
+}
